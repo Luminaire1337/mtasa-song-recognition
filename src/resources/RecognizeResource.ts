b72bba5f8bdaa6cde464cd80feab9245e22c3f94
pkfln <peekfalone@outlook.com>
2021-01-21 00:30:27 +0100

    Using preformatted responses

--- src/resources/RecognizeResource.ts
+++ src/resources/RecognizeResource.ts
@@ -1,12 +1,18 @@
-import { Drash } from '../deps.ts';
+import { Drash, fs } from '../deps.ts';
 
-export default class RecognizeResource extends Drash.Http.Resource {
+import BaseResource from '../BaseResource.ts';
+
+export default class RecognizeResource extends BaseResource {
   static paths = ['/v1/recognize/:mtaresource'];
 
-  public GET() {
-    this.response.body = {
+  public async PUT(): Promise<Drash.Http.Response> {
+    // TODO: Do we need extra security here so the user can't escape the resource folder?
+    // (shouldn't really matter tho, since it's running in a container and requests come from the gameserver only...)
+    if (!(await fs.exists(`/mta_resources/${this.request.getPathParam('mtaresource')}`)))
+      return this.errorResponse(404, 'MTA resource was not found.');
+
+    return this.successResponse(200, {
       ping: 'pong',
-    };
-    return this.response;
+    });
   }
 }
